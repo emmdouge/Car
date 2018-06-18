bf8f06f9b9d525befed76ea1a419549906a9b931
Emmanuel Douge <emmdouge@gmail.com>
2018-06-18 14:48:08 -0400

    complete actions for people

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -1,2 +1,46 @@
 class PersonController < ApplicationController
+    # Remove later
+    # Make test for post to http://localhost:3000/person
+    skip_before_action :verify_authenticity_token
+    
+    # returns all people
+    def index
+        @people = Person.all
+        render json: @people
+    end
+
+    # returns one person
+    def show
+       @person = Person.find(params[:id])
+       render json: @person
+    end
+
+    # returns the page to enter info for new person
+    def new
+        
+    end
+
+    # POST http://localhost:3000/person
+    def create
+       @person = Person.new
+       @person.name = params[:name]
+       @person.email = params[:email]
+       @person.phone = 1112223333
+       @person.save
+    end
+
+    # PATCH http://localhost:3000/person/:id
+    def update
+        @person = Person.find(params[:id])
+        @person.name = params[:name]
+        @person.email = params[:email]
+        @person.phone = params[:phone]
+        @person.save
+    end
+
+    # DELETE http://localhost:3000/person/:id
+    def destroy
+        @person = Person.find(params[:id])
+        @person.destroy
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-  get '/person/:id' => 'person#show'
+  resources :person
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
